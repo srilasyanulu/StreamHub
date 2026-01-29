b475cb2ba75ad4425c01c515cacf3407ddc46cc2
srilasya_nulu <lasyanululasyanulu@gmail.com>
2026-01-29 21:46:07 +0530

    Delete Lasya directory

--- Lasya/js/player.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const videoUrl = params.get("video");
-const title = params.get("title");
-
-const player = document.getElementById("player");
-const movieTitle = document.getElementById("movie-title");
-
-player.src = videoUrl;
-movieTitle.innerText = title;
